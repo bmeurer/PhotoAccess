12cda049e3275f64fc46b09039248669356b1e47
Benedikt Meurer <benedikt.meurer@googlemail.com>
2011-06-26 10:23:01 +0200

    Initial Commit

---
+++ PhotoAccess/PhotoAccessAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  PhotoAccessAppDelegate.h
+//  PhotoAccess
+//
+//  Created by Benedikt Meurer on 6/26/11.
+//  Copyright 2011 Universität Siegen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoAccessAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
